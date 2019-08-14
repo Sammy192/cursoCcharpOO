2daf04cd34d2a1335696de6df797e7d37b0c58b0
Samuel Borges <sammy_rb@hotmail.com>
2019-08-14 19:29:40 -0300

    adicionado exerc de enum

---
+++ Curso cCharp_ OO/secao 09/Course/Course/Program.cs
@@ -0,0 +1,29 @@
+﻿using Course.Entities;
+using Course.Entities.Enums;
+using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            Order order = new Order {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            //de enum para string
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+            // de string para enum
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+
+
+
+        }
+    }
+}
