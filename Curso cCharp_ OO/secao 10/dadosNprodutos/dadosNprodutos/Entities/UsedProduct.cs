bd5eec198febbe1c417b8e9b09f3b683d6455fe3
Samuel Borges <sammy_rb@hotmail.com>
2019-08-20 18:07:53 -0300

    adicionado exer fixacao sobre herancaPolimorfismo

---
+++ Curso cCharp_ OO/secao 10/dadosNprodutos/dadosNprodutos/Entities/UsedProduct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace dadosNprodutos.Entities {
+    class UsedProduct : Product {
+
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct() {
+        }
+
+        public UsedProduct(string name, double price, DateTime manufacureDate) :base(name,price) {
+            ManufactureDate = manufacureDate;
+        }
+
+        public override string PriceTag() {
+            return Name
+            + " (Used) $"            
+            + Price.ToString("F2", CultureInfo.InvariantCulture)
+            + " (Manufacture date: "
+            + ManufactureDate.ToString("dd/MM/yyyy")
+            + ")";
+        }
+
+    }
+}
---
+++ Curso cCharp_ OO/secao 10/dadosNprodutos/dadosNprodutos/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using dadosNprodutos.Entities;
+using System.Collections.Generic;
+
+namespace dadosNprodutos {
+    class Program {
+        static void Main(string[] args) {
+
+            List<Product> List = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i=1; i <= n; i++) {
+                Console.WriteLine($"Enter with date of product: {i}");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char op = char.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string nameX = Console.ReadLine();
+                Console.Write("Price: ");
+                double priceX = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                if (op == 'i') {
+                    Console.Write("Customs fee: ");
+                    double customFeex = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                    List.Add(new ImportedProduct(nameX,priceX,customFeex));
+                }
+                else if (op == 'c') {
+                    List.Add(new Product(nameX, priceX));
+                }
+                else {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime dateX = DateTime.Parse(Console.ReadLine());
+                    List.Add(new UsedProduct(nameX,priceX,dateX));
+                }
+                Console.WriteLine("--------------------------------------------------------");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("Details of price tags: ");
+            foreach (Product pro in List) {
+                Console.WriteLine("--------------------------------------------------------");
+                Console.WriteLine(pro.PriceTag());
+            }
+            Console.WriteLine();
+
+
+        }
+    }
+}
