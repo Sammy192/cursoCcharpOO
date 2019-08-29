b087cb8566d67363fded4e67ad33125a7914948c
Samuel Borges <sammy_rb@hotmail.com>
2019-08-29 10:09:35 -0300

    comecando a implementar um método que imprime o tabuleiro em tela

--- Curso cCharp_ OO/secao 12 jogoXadres/jogoXadres/jogoXadres/Tabuleiro/Tabuleiro.cs
+++ Curso cCharp_ OO/secao 12 jogoXadres/jogoXadres/jogoXadres/Tabuleiro/Tabuleiro.cs
@@ -14,5 +14,11 @@ namespace tabuleiro {
             Colunas = colunas;
             this.pecas = new Peca[linhas,colunas];
         }
+
+        public Peca peca(int linha, int coluna) {
+            return pecas[linha, coluna];
+        }
+
+
     }
 }
