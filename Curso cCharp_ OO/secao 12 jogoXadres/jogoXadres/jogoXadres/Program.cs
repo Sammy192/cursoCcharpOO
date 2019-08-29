6ad2452443e289523c305790e86007594a6569cb
Samuel Borges <sammy_rb@hotmail.com>
2019-08-29 19:41:11 -0300

    colocando excecao personalizada

--- Curso cCharp_ OO/secao 12 jogoXadres/jogoXadres/jogoXadres/Program.cs
+++ Curso cCharp_ OO/secao 12 jogoXadres/jogoXadres/jogoXadres/Program.cs
@@ -5,14 +5,19 @@ using xadrez;
 namespace jogoXadres {
     class Program {
         static void Main(string[] args) {
+            try {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Tabuleiro tab = new Tabuleiro(8,8);
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-            tab.ColocarPeca(new Torre(tab,Cor.Preta),new Posicao(0,0));
-            tab.ColocarPeca(new Torre(tab,Cor.Preta), new Posicao(1, 3));
-            tab.ColocarPeca(new Rei(tab,Cor.Preta), new Posicao(2, 4));
 
-            Tela.imprimirTabuleiro(tab);
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
