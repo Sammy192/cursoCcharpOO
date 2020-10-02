156a680f93114b5a815b9b0898d2de208850ece5
Samuel Borges <sammy_rb@hotmail.com>
2020-10-02 09:26:15 -0300

    alinhando coisas

---
+++ Curso cCharp_ OO/interfaces/exerInterface/Course/Course/Entities/Installment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course.Entities {
+    class Installment {
+        public DateTime DueDate { get; set; }
+        public Double Amount { get; set; }
+
+        public Installment(DateTime dueDate, double amount) {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+    }
+}
